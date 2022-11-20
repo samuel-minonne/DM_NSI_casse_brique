04f58aa3d69f4f5982047435df596e9595c61f17
samuel-minonne <79083644+samuel-minonne@users.noreply.github.com>
2022-11-20 17:09:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,91 @@
 import pyxel
+import SamuelHitboxes as hb
+import math
 
-class Jeu:
+#========================================================================parameters===================================================================
+gameTitle = "Casse Briques"
+gameHeight = 200
+gameWidth = 128
+gameScale = 4
+
+minX = 0
+maxX = gameWidth
+minY = 0
+maxY = gameHeight
+
+playerStartX = 64
+playerStartY = 128
+playerLength = 32
+playerHeight = 8
+playerSpeed = 2
+playerColor = 1
+
+ballStartX = 64
+ballStartY = 64
+ballSize = 4
+ballStartAngle = 315 #warning, the y axis is the wrong way
+ballStartSpeed = 2
+ballColor = 10
+
+key_up = pyxel.KEY_Z
+key_down = pyxel.KEY_S
+key_left = pyxel.KEY_Q
+key_right = pyxel.KEY_D
+
+
+#==================================================================classes================================================================
+class Player:
     def __init__(self):
+        self.xpos = playerStartX
+        self.ypos = playerStartY
+        self.length = playerLength
+        self.height = playerHeight
+        self.hitbox = hb.Hitbox(self.xpos,self.ypos,self.length,self.height)
+        
+    def playerMovement(self):
+        """modifies the values of xspeed and yspeed based on key imputs"""
+        # reading the imputs and converting them to speed
+        if pyxel.btn(key_right) and self.xpos+self.length<maxX:
+            self.xpos += playerSpeed
+            
+        if pyxel.btn(key_left) and self.xpos>minX:
+            self.xpos -= playerSpeed
+            
+        self.hitbox.moveTo(self.xpos,self.ypos)
+        
+class Ball:
+    def __init__(self) -> None:
+        self.xpos = ballStartX
+        self.ypos = ballStartY
+        self.angle = ballStartAngle
+        self.speed = ballStartSpeed
+        self.length = ballSize
+        self.height = ballSize
+        self.hitbox = hb.Hitbox(self.xpos,self.ypos,self.length,self.height)
+        
+    def bouce(self,wallAngle):
+        """takes the angle of the wall as input and makes the ball bounce"""
+        
+        
+    def ballMovement(self):
+        ball.xpos += math.cos(math.radians(self.angle))*self.speed
+        ball.ypos += math.sin(math.radians(self.angle))*self.speed
+        
 
-        # taille de la fenetre 128x128 pixels
-        # ne pas modifier
-        pyxel.init(128, 128, title="Nuit du c0de")
+            
+pyxel.init(gameWidth, gameHeight, title=gameTitle, display_scale=gameScale)
 
-        # position initiale du vaisseau
-        # (origine des positions : coin haut gauche)
-        self.vaisseau_x = 60
-        self.vaisseau_y = 60
+player = Player()
+ball = Ball()
 
-        pyxel.run(self.update, self.draw)
-
-
-    def vaisseau_deplacement(self):
-        """déplacement avec les touches de directions"""
-
-        if pyxel.btn(pyxel.KEY_RIGHT) and self.vaisseau_x<120:
-            self.vaisseau_x += 1
-        if pyxel.btn(pyxel.KEY_LEFT) and self.vaisseau_x>0:
-            self.vaisseau_x += -1
-        if pyxel.btn(pyxel.KEY_DOWN) and self.vaisseau_y<120:
-            self.vaisseau_y += 1
-        if pyxel.btn(pyxel.KEY_UP) and self.vaisseau_y>0:
-            self.vaisseau_y += -1
-
-
-    # =====================================================
-    # == UPDATE
-    # =====================================================
-    def update(self):
-        """mise à jour des variables (30 fois par seconde)"""
-
-        # deplacement du vaisseau
-        self.vaisseau_deplacement()
-
-
-    # =====================================================
-    # == DRAW
-    # =====================================================
-    def draw(self):
-        """création et positionnement des objets (30 fois par seconde)"""
-
-        # vide la fenetre
-        pyxel.cls(0)
-
-        # vaisseau (carre 8x8)
-        pyxel.rect(self.vaisseau_x, self.vaisseau_y, 8, 8, 1)
-
-Jeu()
+def update():
+    player.playerMovement()
+    ball.ballMovement()
+    
+def draw():
+    pyxel.cls(0)
+    pyxel.rect(player.xpos,player.ypos,player.length,player.height,playerColor)
+    pyxel.rect(ball.xpos,ball.ypos,ball.length,ball.height,ballColor)
+    
+    
+pyxel.run(update, draw)
